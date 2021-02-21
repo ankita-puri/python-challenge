e167aeda22dec84783f50e9669a0856afd71857b
Ankita Puri <pr23.ankita@gmail.com>
2021-02-22 02:06:07 +1100

    total amount

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,28 @@
 import os
 import csv
-csvpath = os.path.join('..', 'Resources', 'pybank.csv')
+from typing import Counter
+csvpath = os.path.join('..', 'Resources', 'PyBank.csv')
+# with open(csvpath) as fin:
+#     headerline = fin.next()
+#     total = 0
+#     for row in csv.reader(fin):
+#         total += int(row[1])
+#     print (total)
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
+    csv_header = next(csvreader)
+
+    total = 0
+    for row in csvreader:
+        total += int(row[1])
+    print (total)
+    print(f"CSV Header: {csv_header}")
+    
+    # row_count = sum(1 for row in csvfile)
+    # print(row_count )
+
+# total = 0
+# for row in csvreader:  
+#    total += int(row[1])
+#    print(total)

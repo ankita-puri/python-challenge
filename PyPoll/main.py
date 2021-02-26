8c9f1f2fe46202f529506baad9def854f055feb8
Ankita Puri <pr23.ankita@gmail.com>
2021-02-27 00:29:11 +1100

    MVP with append

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,58 +1,61 @@
 import os
 import csv
-from collections import Counter 
-import operator#to calculate average
+from collections import Counter
+import operator  # to calculate average
 
-#def Average(list):
-   #return round(stat.mean(list),2)
+# def Average(list):
+# return round(stat.mean(list),2)
 
-electionfile = os.path.join('..','Resources','PyPoll.csv')#realtive paths
-resultfile=os.path.join('..','analysis','result.txt')
-#Voter ID,County,Candidate
-VoterID=[] #creating lists to add data 
-County=[]
-CandidateList=[]
-List_of_candidates=[]
-New_List=[]
+electionfile = os.path.join('..', 'Resources', 'PyPoll.csv')  # realtive paths
+resultfile = os.path.join('..', 'analysis', 'result.txt')
+# Voter ID,County,Candidate
+VoterID = []  # creating lists to add data
+County = []
+CandidateList = []
+List_of_candidates = []
+resultDict = {}
+winner_List = []
 
-with open(electionfile, 'r') as PyPoll: #reading datafile
-    reader=csv.reader(PyPoll, delimiter=",")
-    
-    header=next(reader)
+with open(electionfile, 'r') as PyPoll:  # reading datafile
+    reader = csv.reader(PyPoll, delimiter=",")
 
-    for row in reader: 
-        VoterID.append(int(row[0])) 
+    header = next(reader)
+
+    for row in reader:
+        VoterID.append(int(row[0]))
         County.append(row[1])
         CandidateList.append(row[2])
 
-#TotalVotes = 0
-
-#TotalVotes =  print(f"Total Votes:  {len(VoterID)}")
-
 
 List_of_candidates = dict(Counter(CandidateList))
-#print (List_of_candidates) 
-
+# print (List_of_candidates)
+Winner = max(List_of_candidates.items(), key=operator.itemgetter(1))[0]
 s = sum(List_of_candidates.values())
+electionAnalysisTop = (f"Election Results\n"
+                       f"-----------------------------\n"
+                       f"Total Votes:  {len(VoterID)}\n"
+                       f"------------------------------"
+
+                       )
+result_file = open(resultfile, "a")
+
+result_file.write(f"{electionAnalysisTop}"+"\n")
+
+electionAnalysisBottom = (f"------------------------------\n"
+                          f"Winner:{Winner}\n"
+                          f"------------------------------\n"
+                          )
+
+print(electionAnalysisTop)
 for k, v in List_of_candidates.items():
-    pct = round((v * 100.0 / s),2)
-    print(k,pct,v)
+    pct = round((v * 100.0 / s), 2)
+    winnerData = f"{k}"+": "+f"{pct}"+"00%" + " ("+f"{v}"+")"
+    result_file.write(f"{winnerData}"+"\n")
+    print(winnerData)
 
+print(electionAnalysisBottom)
+result_file.write(electionAnalysisBottom)
 
+result_file.close()
 
-
-
-Winner= max(List_of_candidates.items(), key=operator.itemgetter(1))[0]
-#print(Winner)
-
-#electionanalysis=(f"Election Results\n"
-          #f"-----------------------------\n"
-          #f"Total Votes:  {len(VoterID)}\n"
-          
-        
-          #f"------------------------------\n"
-          #f"------------------------------\n"
-          #f"Winner:{Winner}\n"
-#)
-
-#print(electionanalysis)
+# print(electionanalysis)
